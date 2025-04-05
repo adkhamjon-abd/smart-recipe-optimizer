c29d9975e5a498c2ddcc81648c8e1553ef9306d0
Hp <adkhamjon.abd@gmail.com>
2025-04-05 15:44:08 +0200

    Initial Spring Boot project setup

---
+++ recipe-optimizer-backend/src/main/java/com/recipe/optimizer/recipe_optimizer/RecipeOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.optimizer.recipe_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeOptimizerApplication.class, args);
+	}
+
+}
---
+++ recipe-optimizer-backend/src/test/java/com/recipe/optimizer/recipe_optimizer/RecipeOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.optimizer.recipe_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
